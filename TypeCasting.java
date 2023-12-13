e309358da55e5b62c62d41ad233a39769197f0a4
khushbu <khushbusoni475@gmail.com>
2023-12-13 15:23:47 +0530

    [khushbu] adding type casting

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+package OopsProgramLevel2;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        int sum = (int)a + (int)b;
+        System.out.println("Addition of given two numbers: " + sum);
+    }
+}
